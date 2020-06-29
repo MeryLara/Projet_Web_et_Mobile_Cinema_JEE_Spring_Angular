fc774255ec87e383b6349ee6afaecd8ed2db26b5
Amchayd Mariam <meriam.amchayd@gmail.com>
2020-06-29 17:21:44 +0100

    metier and interface

---
+++ src/main/java/org/sid/cinema/entities/Ticket.java
@@ -0,0 +1,32 @@
+package org.sid.cinema.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor @ToString
+public class Ticket implements Serializable {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	private String nomCllient;
+	private double prix;
+	@Column(unique = true)
+	private int codePayment;
+	private boolean reserve;
+	@ManyToOne
+	private Place place;
+	@ManyToOne
+	private Projection projection;
+
+}
